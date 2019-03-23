2186933d8da43c2cb98d744ba4236d40e74613a6
Robert Tyree <tyreer@gmail.com>
2019-03-23 18:28:21 +0000

    move global styles to own file

---
+++ src/Layout/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  html {
+   /* Allow rem units to map to pixel values to facilitate font enlargement for accessibility */
+   font-size: 62.5%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,29 +1,13 @@
 import React from "react";
 import { node } from "prop-types";
-import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyles from "./GlobalStyles";
 
 const theme = {
   wideMedia: "min-width: 600px",
   maxWidth: "1000px",
   gutter: "2rem"
 };
-
-const GlobalStyle = createGlobalStyle`
-  html {
-   /* Allow rem units to map to pixel values to facilitate font enlargement for accessibility */
-   font-size: 62.5%;
-  }
-
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`;
 
 const LayoutStyles = styled.div`
   background: lightsteelblue;
@@ -41,7 +25,7 @@ const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <>
-        <GlobalStyle />
+        <GlobalStyles />
         <LayoutStyles>{children}</LayoutStyles>
       </>
     </ThemeProvider>
